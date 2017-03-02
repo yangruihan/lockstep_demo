4743695a9224b8ae72c08b0d8a0b09c719dc8d9d
C.y <yangruihan@vip.qq.com>
2017-03-02 20:28:52 +0800

    Add:   1. 单例抽象类   2. 对象池

---
+++ Assets/Scripts/Common/InstancePool.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// 可入对象池接口
+/// </summary>
+public interface IPoolable
+{
+
+    void OnAwakeFromPool();
+
+    void OnReturnToPool();
+}
+
+/// <summary>
+/// 对象池
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class InstancePool<T> where T : IPoolable, new()
+{
+    protected Stack<T> _instances;
+
+    public InstancePool(int preSize = 0)
+    {
+        _instances = new Stack<T>();
+        for (int i = 0; i < preSize; i++)
+        {
+            T instance = default(T);
+            _instances.Push((instance == null) ? new T() : instance);
+        }
+    }
+
+    public T Get()
+    {
+        T instance;
+        if (IsEmpty())
+        {
+            instance = default(T);
+            instance = (instance == null) ? new T() : instance;
+        }
+        else
+        {
+            instance = _instances.Pop();
+        }
+
+        instance.OnAwakeFromPool();
+        return instance;
+    }
+
+    public void Recycle(T instance)
+    {
+        if (_instances.Contains(instance))
+        {
+            instance = default(T);
+        }
+        else
+        {
+            instance.OnReturnToPool();
+            _instances.Push(instance);
+        }
+    }
+
+    private bool IsEmpty()
+    {
+        return _instances.Count == 0;
+    }
+}
---
+++ Assets/Scripts/Common/Singleton.cs
@@ -0,0 +1,32 @@
+﻿/// <summary>
+/// 单例抽象类
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class Singleton<T> where T : class, new()
+{
+    private static object _lockObject = new object();
+    protected static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+                lock (_lockObject)
+                {
+                    if (_instance == null)
+                        _instance = new T();
+                }
+
+            return _instance;
+        }
+    }
+
+    public void Free()
+    {
+        _instance = null;
+        FreeSingleton();
+    }
+
+    protected virtual void FreeSingleton() { }
+}
