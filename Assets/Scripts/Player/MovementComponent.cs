8b2084bfbc8423c84c67289e69407eff0ca23b49
C.y <yangruihan@vip.qq.com>
2017-03-04 16:56:25 +0800

    添加移动实现

--- Assets/Scripts/Player/MovementComponent.cs
+++ Assets/Scripts/Player/MovementComponent.cs
@@ -5,8 +5,15 @@
 /// </summary>
 public class MovementComponent : MonoBehaviour
 {
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
     public void OnMoveMsgReceived(MoveMessage msg)
     {
-        Debug.Log("OnMoveMsgReceived: " + msg.PlayerId + " (" + msg.X + ", " + msg.Y + ")");
+        rigidBody.AddForce(new Vector2(msg.X, msg.Y));
     }
 }
